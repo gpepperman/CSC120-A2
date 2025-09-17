12ca8e217eb35a6e11c072eaab3ad60a767bb275
gpepperman <gpepperman@smith.edu>
2025-09-17 09:09:22 -0400

    still fighting

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,11 +1,27 @@
 class ResaleShop:
 
     # What attributes will it need?
-    inventory: float #how many computers does the store own?
-    
+    inventory: int #how many computers does the store own?
+
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, inventory: int):
+        self.inventory = inventory 
 
+    def BuyComputer(self, amt: int):
+        self.inventory += amt
+    
+    def SellComputer(self, amt):
+        self.inventory -= amt
+    
+    #def Refurbish():
+    
+def main():
+    CurrentInventory: ResaleShop = ResaleShop(10)
+    CurrentInventory.BuyComputer(10)
+    CurrentInventory.SellComputer(5)
+    print(CurrentInventory.inventory)
     # What methods will you need?
+
+if __name__ == "__main__":
+    main()
